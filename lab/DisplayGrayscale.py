898333a4c06bf1c5939f8a49958d1d7d9852fcaa
stephypy <stephypy00@gmail.com>
2020-10-31 15:53:12 -0600

    Template for lab

---
+++ lab/DisplayGrayscale.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+'''
+@author: Stephanie Galvan
+@course: Theory of Operating Systems
+@assignment: 3 - Threaded Video Player
+@python-version: 3.6.5
+'''
+
+
+def extract_frames():
+    '''
+    Extracts a series of frames given a video
+    '''
+    pass
+
+
+def convert_grayscale():
+    '''
+    Converts frames to grayscale
+    '''
+    pass
+
+
+def display_frames():
+    '''
+    Display frames as if they were a video
+    '''
+    pass
+
+
+def start():
+    '''
+    Start of code
+    '''
+    pass
+
+
+if __name__ == "__main__":
+    start()
