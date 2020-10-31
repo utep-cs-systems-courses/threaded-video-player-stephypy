dc1f24bc0fdc93865c65417596051e3cb86e459d
stephypy <stephypy00@gmail.com>
2020-10-31 16:09:11 -0600

    Template for functions display and extract frames

--- lab/DisplayGrayscale.py
+++ lab/DisplayGrayscale.py
@@ -7,12 +7,33 @@
 @python-version: 3.6.5
 '''
 
+import cv2
 
-def extract_frames():
+FILE_NAME = '../clip.mp4'
+
+def extract_frames(frame_queue):
     '''
     Extracts a series of frames given a video
     '''
-    pass
+    # Initialize frame count
+    count = 0
+
+    # Open video file
+    vid_cap = cv2.VideoCapture(FILE_NAME)
+
+    # Read first image
+    success, image = vid_cap.read()
+
+    print(f'Reading frame {count} {success}')
+    while success:
+        # TODO: Add the frame to the buffer
+        # frame_queue put image
+
+        success, image = vid_cap.read()
+        print(f'Reading frame {count} {success}')
+        count += 1
+
+    print('Frame extraction complete')
 
 
 def convert_grayscale():
@@ -22,11 +43,31 @@ def convert_grayscale():
     pass
 
 
-def display_frames():
+def display_frames(all_frames):
     '''
     Display frames as if they were a video
     '''
-    pass
+    # Initialize frame count
+    count = 0
+
+    # Go through each frame in the buffer until the buffer is empty
+    while not all_frames.empty():
+        # TODO: Get the next frame
+        # frame = dequeue all_frames
+
+        print(f'Displaying frame {count}')
+
+        # Display the image in a window called "video" and wait 42ms
+        # before displaying the next frame
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(42) and 0xFF == ord("q"):
+            break
+
+        count += 1
+
+    print('Finished displaying all frames')
+    # cleanup the windows
+    cv2.destroyAllWindows()
 
 
 def start():
